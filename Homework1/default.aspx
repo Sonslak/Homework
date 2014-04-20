e51823cab994894f07fe3ac0a5130618e4c8fc12
Sonslak <sonialouw.london@gmail.com>
2014-04-20 17:34:34 +0100

    new

--- Homework1/default.aspx
+++ Homework1/default.aspx
@@ -8,15 +8,22 @@
     <link href="css/main.css" rel="stylesheet" />
 </head>
 <body>
+    <div class="container">
+        <h1>Cergis Memory Game</h1>
+        
 
-    <ul id="people">
-    </ul>
+         <h2><label id="timer" data-seconds="0" ></label></h2>
 
+        <input type="button" value="Start Game" id="btnStartGame" onclick="startGame()" />
+        <ul id="people" class="hide">
+        </ul>
+        
+    </div>
     <input type="hidden" id="prev-id" value="0" />
     <input type="hidden" id="click-count" value="0" />
     <input type="hidden" id="click-enabled" value="true" />
 
-  
+
 
     <!-- jquery -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
--- Homework1/js/global.js
+++ Homework1/js/global.js
@@ -1,19 +1,56 @@
-﻿$(document).ready(function () {
+﻿var handle;
+
+function startGame() {
+
+    // change text
+    $("#btnStartGame").prop('value', 'Restart Game');
 
     // get all images in folder
+    $("#people").html("");
+    $("#people").removeClass("hide");
     getImages();
+   
+    resetTimer(handle);
 
-});
+    // start timer
+    handle = setInterval(function () {modifyTimer()}, 1000);
+}
 
+function modifyTimer() {
+
+    if ($('.unmatched').length > 0) {
+        var seconds = $('#timer').attr('data-seconds');
+        $("#timer").attr('data-seconds', parseInt(seconds, 0) + 1);
+        $("#timer").html(formatTime(parseInt(seconds, 0) + 1));
+    }
+    else
+    {
+        alert("Done!");
+        clearInterval(handle);
+    }
+}
+
+function resetTimer(id)
+{
+    clearInterval(id);
+    $('#timer').attr('data-seconds', 0);
+    $("#timer").html(formatTime(0));
+}
+
+function formatTime(s) {
+    var h = Math.floor(s / 3600); // hours
+    s -= h * 3600;
+    var m = Math.floor(s / 60); // minutes
+    s -= m * 60;
+    return (h < 10 ? '0' + h : h) + ":" + (m < 10 ? '0' + m : m) + ":" + (s < 10 ? '0' + s : s); //zero padding
+}
 
 $('#people').on('click', '.person', function (event) {
 
-
     // prevent default action
     event.preventDefault();
-  
-    if ($("#click-enabled").attr('value') == "true")
-    {
+
+    if ($("#click-enabled").attr('value') == "true") {
         $("#click-enabled").attr('value', "false");
 
         var that = $(this);
@@ -26,17 +63,11 @@ $('#people').on('click', '.person', function (event) {
         var counter = $("#click-count").attr('value');
         counter++;
         $("#click-count").attr('value', counter);
-
-        //// values
-        console.log("current:" + that.attr("data-img"));
-        console.log("prev: " + prev.attr('value') + " " + prev.children('img').attr("src"));
-
-        if (counter >= 2)
-        {
+        if (counter >= 2) {
             if (counter == 2) {
-              
                 if (that.attr("data-img") == prev.children('img').attr("src")) {
-
+                    that.children('img').removeClass('unmatched');
+                    prev.children('img').removeClass('unmatched');
                     that.children('img').addClass('matched');
                     prev.children('img').addClass('matched');
                 }
@@ -44,28 +75,23 @@ $('#people').on('click', '.person', function (event) {
 
             // set prev clicked
             prev.attr('value', that.attr('id'));
-
             setTimeout('resetAll()', 500);
-
         }
-        else
-        {        
+        else {
             $("#click-enabled").attr('value', "true");
 
             // set prev clicked
             prev.attr('value', that.attr('id'));
         }
-   
-        $("#prev-id").attr('value', $(this).attr('id'));     
+
+        $("#prev-id").attr('value', $(this).attr('id'));
     }
 });
 
 
-function resetAll()
-{
+function resetAll() {
     $("#people").find("img").map(function () {
-        if (!$(this).hasClass('matched'))
-        {
+        if (!$(this).hasClass('matched')) {
             $(this).attr('src', "img/peek.png");
         }
     })
@@ -73,13 +99,14 @@ function resetAll()
     $("#click-last").attr('value', "0");
     $("#click-count").attr('value', "0");
     $("#click-enabled").attr('value', "true");
-}
 
+
+}
 
 function getImages() {
     $.ajax({
         //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-        url: getRootURL() +  "/img/memory-game",
+        url: getRootURL() + "/img/memory-game",
         success: function (data) {
 
             var arr = [];
@@ -90,12 +117,12 @@ function getImages() {
                 arr.push(this.href);
                 arr.push(this.href);
             });
-        
+
             // shuffle
             var suffleArr = shuffleArray(arr);
             $.each(suffleArr, function (index, value) {
-                
-                $("#people").append($("<li><a id=" + index + " class='person' data-img=" + value + " href='#'><img width='100%' src='img/peek.png'></img></a></li>"));
+
+                $("#people").append($("<li><a id=" + index + " class='person' data-img=" + value + " href='#'><img width='100%' class='unmatched' src='img/peek.png'></img></a></li>"));
             });
 
         }
@@ -131,4 +158,4 @@ function getRootURL() {
 
 
 
- 
+
